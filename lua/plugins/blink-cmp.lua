94a384ab00bb957e6242fa35bcac4e87b78efd50
Fraz Mahmud <fraz501@hotmail.com>
2025-07-25 16:17:54 -0400

    function snippets will now show function signatures

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,22 +1,22 @@
 return {
-    'saghen/blink.cmp',
+    "saghen/blink.cmp",
     -- optional: provides snippets for the snippet source
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = { "rafamadriz/friendly-snippets" },
 
     -- use a release tag to download pre-built binaries
-    version = '1.*',
+    version = "1.*",
     -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
+    -- build = "cargo build --release",
     -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
+    -- build = "nix run .#build-plugin",
 
-    -- @module 'blink.cmp'
+    -- @module "blink.cmp"
     -- @type blink.cmp.Config
     opts = {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-        -- 'super-tab' for mappings similar to vscode (tab to accept)
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
+        -- "default" (recommended) for mappings similar to built-in completions (C-y to accept)
+        -- "super-tab" for mappings similar to vscode (tab to accept)
+        -- "enter" for enter to accept
+        -- "none" for no mappings
         --
         -- All presets have the following mappings:
         -- C-space: Open menu or open docs if already open
@@ -25,12 +25,12 @@ return {
         -- C-k: Toggle signature help (if signature.enabled = true)
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = 'default' },
+        keymap = { preset = "default" },
 
         appearance = {
-            -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- "mono" (default) for "Nerd Font Mono" or "normal" for "Nerd Font"
             -- Adjusts spacing to ensure icons are aligned
-            nerd_font_variant = 'mono'
+            nerd_font_variant = "mono"
         },
 
         -- (Default) Only show the documentation popup when manually triggered
@@ -55,7 +55,8 @@ return {
         -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
         --
         -- See the fuzzy documentation for more information
-        fuzzy = { implementation = "prefer_rust_with_warning" }
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+        signature = { enabled = true };
     },
     opts_extend = { "sources.default" }
 }
