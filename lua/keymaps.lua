cdd18a462e4f5a751b44eaafb4ecc758d60e91aa
Fraz Mahmud <fraz501@hotmail.com>
2025-07-28 16:01:50 -0400

    increased terminal height

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -24,7 +24,7 @@ vim.keymap.set("n", "<leader>1", function()
     vim.cmd.new()
     vim.cmd.term()
     vim.cmd.wincmd("J")
-    vim.api.nvim_win_set_height(0, 15)
+    vim.api.nvim_win_set_height(0, 30)
 end)
 
 -- Keymap to leave terminal easier
